6dc53b374ea4ac87e9533118a2136d63a1070b99
Nicolò Vescera <nicolo.vescera@gmail.com>
2025-09-16 07:51:05 +0000

    ✨ (run.sh): check for new rules (#1)
    * ✨ (run.sh): check for new rules
    
    Added code to check if a newer set of rules is present inside the remote
    github repo. After every sleep, it checks if the MD5 hash is different
    from the remote file and then downloads newer file.
    
    Added python code execution after rules check.
    
    * Update run.sh
    
    ---------
    
    Co-authored-by: Mirko Mariotti <mirko@mirkomariotti.it>

--- run.sh
+++ run.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-while true:
+LOCAL_FILE=rules.yaml
+REMOTE_URL=https://raw.githubusercontent.com/UniNuvola/promflux/refs/heads/main/rules.yaml
+
+checkrules()
+{
+	# compute local MD5
+	LOCAL_MD5=$(md5sum "$LOCAL_FILE" | awk '{ print $1 }')
+
+	# compute remote MD5
+	REMOTE_MD5=$(curl -sL "$REMOTE_URL" | md5sum | awk '{ print $1 }')
+
+	# compare hashes
+	if [[ "$LOCAL_MD5" == "$REMOTE_MD5" ]]; then
+    	echo "Files are the same."
+	else
+    	echo "Files are different."
+    	echo "Downloading new rules ..."
+    	curl -o rules.yaml $REMOTE_URL 
+	fi
+}
+
+while true
 do
-	sleep 10
+	sleep 3600
+	checkrules	
+	python3 promflux.py
 done
